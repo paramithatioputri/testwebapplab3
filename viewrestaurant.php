af5e0c749c941183e9d2b348aa1c507def57a3d9
paramithatioputri <53428914+paramithatioputri@users.noreply.github.com>
2020-06-11 14:25:48 +0700

    Update viewrestaurant.php

--- viewrestaurant.php
+++ viewrestaurant.php
@@ -1,13 +1,19 @@
 <?php
   $serverName = "simplewebtp047112.database.windows.net";
+
   $connectionOptions = array(
   "Database" => "simpleweb",
   "Uid" => "simpleweb",
   "PWD" => "Tp047112");
+
   //Establishes the connection
   $conn = sqlsrv_connect($serverName, $connectionOptions);
   if (!$conn)
   {
   die("Error connection: ".sqlsrv_errors());
   }
+
+  $tsql= "SELECT * FROM [dbo].[restaurant]";
+  $getResults= sqlsrv_query($conn, $tsql);
+
 ?>
